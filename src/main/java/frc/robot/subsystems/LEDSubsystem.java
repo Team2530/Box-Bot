92e3a9d1fd458f5f6dc89394301e10f94c681ceb
Sunghwan1234 <sunghwan1002222@gmail.com>
2025-01-18 10:22:40 -0600

    LED Stable v1.0 Fixed RGBW LED Issues to make this completly compatible with RGBW.
    Fixes: ledbuffer Length fix

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -17,33 +17,36 @@ public class LEDSubsystem extends SubsystemBase {
 
     public LEDSubsystem() {
         led = new AddressableLED(8);
-        ledCount=40;
+        ledCount = 80;
         RGBWData = new int[ledCount * 4];
-        ledBuffer = new AddressableLEDBuffer(ledCount);
+        ledBuffer = new AddressableLEDBuffer(ledCount+(int) Math.floor(ledCount/3));
         led.setLength(ledBuffer.getLength());
         led.setData(ledBuffer);
         led.start();
     }
     @Override
     public void periodic() {
-        rainbow(tick, 1, 10, 0);
-
+        //for (int i=0;i<ledCount-1;i++) {
+        //    setDataRGBW(i,0,255,0,0);
+        //}
         tick+=1;
-
-        push();
+        //System.out.println(tick+" r: ");
+        rainbow(tick,0.8,1,0);
+        //rainbow2(tick,0.01);
+        
+        pushData();
         led.setData(ledBuffer);
     }
 
     /* Periodic Funcitons */
-    public void rainbow(int tick, int speed, int change, int w) {
-        int rainbow = (tick*speed) % 180;
+    public void rainbow(int tick, double speed, double change, int w) {
+        double rainbow = tick*speed;
         for (int i=0;i<ledCount-1;i++) {
-            Color c = Color.fromHSV(rainbow-(i*change), 255, 255);
-            setDataRGBW(i, (int) c.red, (int) c.green, (int) c.blue, w);
+            Color c = Color.fromHSV((int) (rainbow+(i*change) % 180), 255, 255);
+            setDataRGBW(i, (int) (c.red*255), (int) (c.green*255), (int) (c.blue*255), w);
         }
         //setAllDataRGBW(rc,0);
     }
-
     /* Old Periodic */
     void rainbow1(int tick) {
         for (int i = 0; i < ledBuffer.getLength(); ++i) {
@@ -54,11 +57,11 @@ public class LEDSubsystem extends SubsystemBase {
     public void rainbow2(int tick, double difference) {
         tick*=0.1;
         for (int i=0;i<ledCount-1;i++) {
-            double j = i*(Math.PI-difference);
+            double j = i*difference;
             setDataRGBW(i,
             (int) (Math.sin(tick+j)*255),
-            (int) (Math.sin(tick+2+j)*255),
-            (int) (Math.sin(tick+4+j)*255),0);}
+            (int) (Math.sin(tick+2*j)*255),
+            (int) (Math.sin(tick+3*j)*255),0);}
     }
     boolean toFlash(int tick, int period) {
         int flash = tick % period;
@@ -69,15 +72,14 @@ public class LEDSubsystem extends SubsystemBase {
         double v = ((Timer.getFPGATimestamp()) % period) / period;
         return v < 0.5;
     }
-
     void sineColor(int r, int g, int b, double waves, double center, double amp, double tscroll) {
         double lambda = ledBuffer.getLength() / (2 * waves);
         for (int i = 0; i < 20; ++i) {
             double fx = Math.cos((i / lambda) * Math.PI + Timer.getFPGATimestamp() * tscroll * Math.PI) * amp + center;
-            setRGB(i,
+            setDataRGBW(i,
                     Math.min((int) (r * fx), 255),
                     Math.min((int) (g * fx), 255),
-                    Math.min((int) (b * fx), 255));
+                    Math.min((int) (b * fx), 255),0);
         }
     }
 
@@ -95,36 +97,47 @@ public class LEDSubsystem extends SubsystemBase {
         RGBWData[(index * 4) + 2] = B;
         RGBWData[(index * 4) + 3] = W;
     }
-    public void setRGBW(int RGBWIndex, int R, int G, int B, int W) {
-        int RGBIndex = RGBWIndex + (int) Math.floor(RGBWIndex/3); // Index offset ONLY FOR ENCODING INDO GRBGRBGRBGRB
-        int[] prevRGBW = getDataRGBW(RGBWIndex);
-        if (RGBWIndex>0) {prevRGBW = getDataRGBW(RGBWIndex-1);}
+    public void setRGBW(int index, int R, int G, int B, int W) {
+        int trueIndex = index + (int) Math.floor(index/3); // Index offset ONLY FOR ENCODING INTO GRBGRBGRBGRB
+        int[] prevRGBW = getDataRGBW(index);
+        if (index>0) {prevRGBW = getDataRGBW(index-1);}
 
-        int[] nextRGBW = getDataRGBW(RGBWIndex+1);
+        int[] nextRGBW = getDataRGBW(index);
+        if (!(index+1>ledCount)) {nextRGBW=getDataRGBW(index+1);}
 
-        //print(prevRGB.toString());
         /** HOW TO READ R G B comments
-         * I will use C and V as example
+         * I will use C (Current) and V (Value) as example
          * C=C: Use C to output as C. basically just normal setRGB
-         * C=V: Use V to output as C. for example G=W (W is the input value from us)
+         * C=V: Use the V to output as C. for example G=W (W is the input value from here)
          * C=+V: Use the next index's V to output
          * C=-V: Use the previous index's V to output.
+         * Lowercase: Previous or After's
          * 
-         * REMEMBER: you use the INDEX, not the OFFSETINDEX
+         * Cases:           case0 case1 case2 case0
+         * YOUR input:      RGB W|RG BW|R GBW|RGBW|... 
+         * case 0:          RGB g Wr          RGB    
+         * case 1:              G wR WB g
+         * case 2:                   wb G BRW    
+         * input:           RGB|R GB|RG B|RGB|RGB|...
+         *                    reorders correctly
+         * setLED:          BGR|B GR|BG R|BGR|BGR|...
+         *                    reorders correctly
+         * WPILIB sends:    GRB|G RB|GR B|GRB|GRB|...
+         * output:          GRB W|GR BW|G RBW|GRBW|...
          */
 
-        switch (RGBWIndex % 3) {
+        switch (index % 3) {
             case 0:
-                ledBuffer.setRGB(RGBIndex, R, G, B); // R=R G=G B=B
-                ledBuffer.setRGB(RGBIndex+1, nextRGBW[1], W, nextRGBW[0]); // R=+G G=W B=+R
+                setRGB(trueIndex, R, G, B); // R=R G=G B=B
+                setRGB(trueIndex+1, nextRGBW[1], W, nextRGBW[0]); // R<+G G<W B<+R
                 break;
             case 1:
-                ledBuffer.setRGB(RGBIndex, G, prevRGBW[3], R); // R=G G>-W B=R
-                ledBuffer.setRGB(RGBIndex+1, W, B, nextRGBW[1]); // R=W G=B B=+G FOR B, REMEMBER THAT THE INDEX is 1
+                setRGB(trueIndex, G, prevRGBW[3], R); // R<G G<-W B<R
+                setRGB(trueIndex+1, W, B, nextRGBW[1]); // R=W G=B B=+G FOR B, REMEMBER THAT THE INDEX is 1
                 break;
             case 2:
-                ledBuffer.setRGB(RGBIndex, prevRGBW[3], prevRGBW[2], G); // R=-W G=-B B=G
-                ledBuffer.setRGB(RGBIndex+1, B, R, W); // R=B G=R B=W
+                setRGB(trueIndex, prevRGBW[3], prevRGBW[2], G); // R=-W G=-B B=G
+                setRGB(trueIndex+1, B, R, W); // R=B G=R B=W
                 break;
             default:break;
         }
@@ -140,9 +153,10 @@ public class LEDSubsystem extends SubsystemBase {
             setColor(i, color);
     }
 
-    public void push() { // This sets the RGBW
+    public void pushData() { // This sets the RGBW
         for (int i=0; i<ledCount-1;i++) {
-            setRGBW(i, RGBWData[i * 4], RGBWData[i * 4 + 1], RGBWData[i * 4 + 2], RGBWData[i * 4 + 3]);}
+            setRGBW(i, RGBWData[i * 4], RGBWData[i * 4 + 1], RGBWData[i * 4 + 2], RGBWData[i * 4 + 3]);
+        }
     }
 
     /* Shortened Names */
